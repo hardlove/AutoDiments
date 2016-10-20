ee8b1bdaa350a766e7847c823ff738acdbdeadf7
chenlu <chenlu@globalroam.com>
2016-10-20 14:10:40 +0800

    通过java -jar xx.jar -uiwidth 1080 -uiheight 1920 -uidensity 320 [720X1080] [320x480] 自动生存尺寸

---
+++ DimentsUtils/src/com/globalroam/diments/DeviceBean.java
@@ -0,0 +1,83 @@
+	package com.globalroam.diments;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeviceBean {
+	Type dpiType;
+	int deviceWidth;
+	int deviceHeight;
+	int density;
+	String dpiName;
+	boolean isStandard = true;
+
+	public enum Type {
+		Ldpi, Mdpi, Hdpi, XHdpi, XXHdpi,NonStandard
+	}
+	
+	public DeviceBean(int deviceWidth, int deviceHeight){
+		this(Type.NonStandard, deviceWidth, deviceHeight, 0, false);
+		
+	}
+
+	private DeviceBean(Type dpiType, int deviceWidth, int deviceHeight,
+			int density,boolean isStandard) {
+		super();
+		this.dpiType = dpiType;
+		this.deviceWidth = deviceWidth;
+		this.deviceHeight = deviceHeight;
+		this.density = density;
+		this.isStandard = isStandard;
+
+		switch (dpiType) {
+
+		case Ldpi:
+			this.dpiName = "ldpi";
+			break;
+		case Mdpi:
+			this.dpiName = "mdpi";
+			break;
+		case Hdpi:
+			this.dpiName = "hdpi";
+			break;
+		case XHdpi:
+			this.dpiName = "xhdpi";
+			break;
+		case XXHdpi:
+			this.dpiName = "xxhdpi";
+			break;
+
+		default:
+			break;
+		}
+		
+		if(!isStandard){
+			this.dpiName = "values-"+deviceHeight+"x"+deviceWidth;
+		}else{
+			this.dpiName = "values-"+this.dpiName;
+		}
+		System.out.println("dipName is:"+dpiName+ " is Standard:"+isStandard+"  "+deviceWidth+"x"+deviceHeight);
+	}
+	
+	/**
+	 * 获取标准分辨率的设备
+	 * @return
+	 */
+	public static List<DeviceBean> getStandardDevices(){
+		int[] deviceWidth   = new int[]{240,320,480,720, 1080};
+    	int[] deviceHeight  = new int[]{320,480,800,1280,1920};
+    	int[] density       = new int[]{120,160,240,320, 480};
+    	Type[] dpiType      = new Type[]{Type.Ldpi,Type.Mdpi,Type.Hdpi,Type.XHdpi,Type.XXHdpi};
+		
+    	List<DeviceBean> devices = new ArrayList<DeviceBean>();
+    	for (int i = 0; i < dpiType.length; i++) {
+    		
+			DeviceBean device = new DeviceBean(dpiType[i], deviceWidth[i], deviceHeight[i], density[i], true);
+			devices.add(device);
+		}
+    	
+		return devices;
+		
+	}
+
+}
